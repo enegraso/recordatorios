c7351a02cf5f04637a2b9e0ab0cf090b8e5795f0
Federico Ortiz <sib2000@gmail.com>
2025-12-12 21:06:37 -0300

    new price promo

--- src/programador.js
+++ src/programador.js
@@ -55,7 +55,7 @@ function programador_tareas() {
         "👋 Hola -NB- app ♾️Mul7ivisi0nPl4y!\n" +
         "Y -CINE- app 🎬🎞️📽️📺 *de regalo*. \n Cómo estás? Aproximadamente en *50 horas* vence tu abono\nDesearía renovar?\n\n" +
         "Precio *$ 12000* y medios de pago: *👉 https://bit.ly/s2kmail 👈* (tap/presionar en el enlace) \n\n" +
-        "🎅 _*PROMO FIESTAS*_ *3mx25000* Consultanos! \n\n" +
+        "🎅 _*PROMO FIESTAS*_ *3mx30.000* Consultanos! \n\n" +
         "Enterate antes: Novedades, actualizaciones de app/precios en 📢 Canal WA: bit.ly/canalwamp \n\n" +
         "*Envíe comprobante de pago*, luego de hacerlo. \n" +
         "📧 Si se vence la cuenta, se perderá el acceso hasta su regeneración. " +
